b65ff4907ad35be8982ffca615b1a5457c046959
Natacha Bouhours <natachabou49@gmail.com>
2017-02-23 00:00:50 +0100

    changement mode bar

--- Cartes/Bar.java
+++ Cartes/Bar.java
@@ -9,7 +9,21 @@ public class Bar extends Carte implements ICarte {
 	@Override
 	public void actionLancee() {
 		try {
-			Game.pile.depiler();
+			boolean periode_saute = false;
+			String next_periode = "";
+			int nb_periode = 0;
+			while (!periode_saute) {
+				ICarte carte = Game.pile.depiler();
+				
+				if (!carte.getM_periode().equals(this.getM_periode()) || nb_periode > 0) {
+					if ((carte.getM_periode().equals("matin") && nb_periode == 0)) {
+						nb_periode++;
+					}
+					else{
+						periode_saute = true;
+					}
+				}
+			}
 		} catch (pileException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
